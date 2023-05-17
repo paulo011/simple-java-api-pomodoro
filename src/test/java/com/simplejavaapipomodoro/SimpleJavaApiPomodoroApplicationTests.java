7b158905633a8306c8d041708eb8e4bdfd39b122
paulo011 <pauloortega011@gmail.com>
2023-05-17 18:28:59 -0300

    Initialize repository

---
+++ src/test/java/com/simplejavaapipomodoro/SimpleJavaApiPomodoroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplejavaapipomodoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleJavaApiPomodoroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
