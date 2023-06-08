56dff5241eb0be434ef74a45eade95d9b70a72a2
paulo011 <pauloortega011@gmail.com>
2023-06-08 18:23:45 -0300

    refactor handleValidationExceptions body

--- src/main/java/com/simplejavaapipomodoro/GlobalExceptionHandlers/GlobalExceptionHandler.java
+++ src/main/java/com/simplejavaapipomodoro/GlobalExceptionHandlers/GlobalExceptionHandler.java
@@ -2,14 +2,11 @@ package com.simplejavaapipomodoro.GlobalExceptionHandlers;
 
 import com.simplejavaapipomodoro.DTO.GlobalErrorDTO;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestControllerAdvice
@@ -17,12 +14,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public GlobalErrorDTO handleValidationExceptions(MethodArgumentNotValidException exception){
-        BindingResult result = exception.getBindingResult();
-        List<FieldError> fieldErrors = result.getFieldErrors();
-        List<String> errors = new ArrayList<>();
-        for(FieldError fieldError : fieldErrors){
-            errors.add(fieldError.getField() + ": " + fieldError.getDefaultMessage());
-        }
+        List<String> errors = exception.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(x -> x.getField() +  ": " + x.getDefaultMessage()).toList();
+
         return new GlobalErrorDTO(HttpStatus.BAD_REQUEST, errors);
     }
 }
