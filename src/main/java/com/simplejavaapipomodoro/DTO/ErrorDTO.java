8e1ec1cb33b025c5793a40744c02dcdc0d1cc9f4
paulo011 <pauloortega011@gmail.com>
2023-06-15 17:17:08 -0300

    new constructor in ErrorDTO

--- src/main/java/com/simplejavaapipomodoro/DTO/ErrorDTO.java
+++ src/main/java/com/simplejavaapipomodoro/DTO/ErrorDTO.java
@@ -2,5 +2,11 @@ package com.simplejavaapipomodoro.DTO;
 
 import org.springframework.http.HttpStatus;
 
-public record ErrorDTO(HttpStatus statusCode, String message) {
+import java.util.Collections;
+import java.util.List;
+
+public record ErrorDTO(HttpStatus httpStatus, List<String> messages) {
+    public ErrorDTO(HttpStatus httpStatus, String messages){
+        this(httpStatus, Collections.singletonList(messages));
+    }
 }
